69ae0bdade2929f7ff301644a8835b54daefe295
amirdehestani <amirmo800@gmail.com>
2025-01-29 16:59:10 +0100

    open posts data from json

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+import json
+
+from flask import Flask ,render_template
 
 app = Flask(__name__)
 
+with open("data.json","r") as file :
+    blog_posts = json.load(file)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # add code here to fetch the job posts from a file
+    return render_template('index.html', posts=blog_posts)
+
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
