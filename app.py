678a9cdc6009caaa5dee9faaa9d7800ab6e9e94d
amirdehestani <amirmo800@gmail.com>
2025-01-29 17:31:09 +0100

    add post function added

--- app.py
+++ app.py
@@ -1,19 +1,42 @@
 import json
 
-from flask import Flask ,render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-with open("data.json","r") as file :
-    blog_posts = json.load(file)
+def load_posts():
+    try:
+        with open("data.json", "r") as file:
+            blog_posts = json.load(file)
+            return blog_posts
+    except:
+        return FileNotFoundError
+
+def save_posts(posts):
+    with open("data.json", "w") as file:
+        json.dump(posts, file, indent=4)
 
 
 @app.route('/')
-def index():
-    # add code here to fetch the job posts from a file
-    return render_template('index.html', posts=blog_posts)
+def home():
+    posts = load_posts()
+    return render_template('index.html', posts=posts)
 
 
+@app.route('/add', methods=['GET', 'POST'])
+def add_post():
+    if request.method == 'POST':
+        posts = load_posts()
+        new_post = {
+            "id": len(posts) + 1,
+            "author": request.form['author'],
+            "title": request.form['title'],
+            "content": request.form['content']
+        }
+        posts.append(new_post)
+        save_posts(posts)
+        return redirect(url_for('home'))
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
